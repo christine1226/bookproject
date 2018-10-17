119d1b3c06ece9900c3f713d9c24a232bb1704ed
github username <github email address>
2018-10-17 14:49:41 -0400

    First commit, created authors MVC, for show, new, create

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,30 @@
+class AuthorsController < ApplicationController
+  def index
+    @authors = Author.all
+  end
+
+  def show
+    @author = Author.find(params[:id])
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(params)
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
