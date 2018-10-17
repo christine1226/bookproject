627b4ffef7429de0be7c852b15f3cda221ca2c35
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-10-17 18:11:50 -0400

    Stuck on show.erb #drop_down_menu

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,38 +4,13 @@ class BooksController < ApplicationController
   end
 
   def show
-    
+    @authors = Author.all
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
+  def deletejoin
 
-  def create
-    @book = Book.new(book_params)
-    @book.save
     redirect_to @book
-  end
-
-  def edit
-    @book = Book.find(params[:id])
-  end
-
-  def update
-    @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to @book
-  end
-
-  def destroy
-
-  end
-
-  private
-
-  def book_params
-    params.require(:book).permit(:title, :pages)
   end
 
 end
